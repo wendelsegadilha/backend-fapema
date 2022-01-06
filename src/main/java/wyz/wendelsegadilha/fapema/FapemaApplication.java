4b685fd3e6618348070684501d55a5967028cf11
Wendel Segadilha <wendelsegadilha99@gmail.com>
2022-01-06 17:41:37 -0300

    startup do projeto

---
+++ src/main/java/wyz/wendelsegadilha/fapema/FapemaApplication.java
@@ -0,0 +1,13 @@
+package wyz.wendelsegadilha.fapema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FapemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FapemaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wyz/wendelsegadilha/fapema/FapemaApplicationTests.java
@@ -0,0 +1,13 @@
+package wyz.wendelsegadilha.fapema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FapemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
