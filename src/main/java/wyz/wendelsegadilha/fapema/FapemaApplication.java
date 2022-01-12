0a0dbd98944342ca663988731d93f55c63acc944
Wendel Segadilha <wendelsegadilha99@gmail.com>
2022-01-12 12:13:49 -0300

    Alteracao do banco de dados para MySQL e versao do SpringBoot

--- src/main/java/wyz/wendelsegadilha/fapema/FapemaApplication.java
+++ src/main/java/wyz/wendelsegadilha/fapema/FapemaApplication.java
@@ -1,23 +1,18 @@
 package wyz.wendelsegadilha.fapema;
 
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 import wyz.wendelsegadilha.fapema.config.ArmazenamentoArquivoConfig;
-import wyz.wendelsegadilha.fapema.domain.Aluno;
-import wyz.wendelsegadilha.fapema.repositories.AlunoRepository;
 
 @SpringBootApplication
 @EnableConfigurationProperties({ArmazenamentoArquivoConfig.class})
 public class FapemaApplication implements CommandLineRunner{
 
-	@Autowired
-	private AlunoRepository alunoRepository;
+	//@Autowired
+	//private AlunoRepository alunoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(FapemaApplication.class, args);
@@ -25,11 +20,12 @@ public class FapemaApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		
-		Aluno aluno01 = new Aluno(null, "Felipe Corte Real", "22608948103", "20201ENG.SIN00001", ""); 
-		Aluno aluno02 = new Aluno(null, "Fernanda Helena Galvão", "64421153214", "20201ENG.SIN00002", ""); 
-		Aluno aluno03 = new Aluno(null, "Sophia Silva Teixeira", "43247439757", "20201ENG.SIN00003", ""); 
+		/*
+		Aluno aluno01 = new Aluno(null, "Bill Gates", "22608948103", "20201ENG.SIN00001", ""); 
+		Aluno aluno02 = new Aluno(null, "Elon Musk", "64421153214", "20201ENG.SIN00002", ""); 
+		Aluno aluno03 = new Aluno(null, "Jack Ma", "43247439757", "20201ENG.SIN00003", ""); 
 		alunoRepository.saveAll(Arrays.asList(aluno01, aluno02, aluno03));
+		*/
 
 	}
 
